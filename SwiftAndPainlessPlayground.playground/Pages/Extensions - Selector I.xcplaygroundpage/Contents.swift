03de55ac95475a00fd5a9733853a73a81b135b41
StuFF mc <mc@stuffmc.com>
2016-12-08 10:23:04 +0100

    Adding a useful demo of using a LiveView with a Tap event

--- SwiftAndPainlessPlayground.playground/Pages/Extensions - Selector I.xcplaygroundpage/Contents.swift
+++ SwiftAndPainlessPlayground.playground/Pages/Extensions - Selector I.xcplaygroundpage/Contents.swift
@@ -1,18 +1,23 @@
 import UIKit
+import PlaygroundSupport
 /*:
  [⬅️](@previous) [➡️](@next)
  # Extensions: Selector I
  */
+let rect = CGRect(x: 0, y: 0, width: 100, height: 50)
+
 class View: UIView {
   
   let button: UIButton
   
   override init(frame: CGRect) {
     button = UIButton(type: .system)
+    button.frame = rect
     button.addTarget(nil, action: #selector(DetailShowable.showDetail), for: .touchUpInside)
     
     super.init(frame: frame)
     // add subview and do layout
+    addSubview(button)
   }
   
   required init?(coder aDecoder: NSCoder) { fatalError() }
@@ -21,3 +26,12 @@ class View: UIView {
 @objc protocol DetailShowable {
   @objc func showDetail()
 }
+
+class MyView: View, DetailShowable {
+    func showDetail() {
+        print("You tapped the black button in the LiveView/Timeline/Assistant Editor. Good boy!")
+    }
+}
+
+PlaygroundPage.current.liveView = MyView(frame: rect)
+PlaygroundPage.current.needsIndefiniteExecution = true
